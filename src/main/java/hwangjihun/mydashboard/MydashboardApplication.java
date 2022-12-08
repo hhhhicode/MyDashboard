e432662c745c4ec8888c964b7e99f0740e4041de
Hwang Ji Hun <hhhhicode@gmail.com>
2022-12-08 19:05:17 +0900

    프로젝트 생ㅅ어

---
+++ src/main/java/hwangjihun/mydashboard/MydashboardApplication.java
@@ -0,0 +1,13 @@
+package hwangjihun.mydashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hwangjihun/mydashboard/MydashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package hwangjihun.mydashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
